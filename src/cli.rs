f94535c6a000a99fb07614d6eede335d0dadd6a6
uimaxbai <uimax.bai@gmail.com>
2024-03-14 17:22:52 +0000

    f

--- src/cli.rs
+++ src/cli.rs
@@ -3,6 +3,7 @@ use clap_complete::Shell;
 
 pub fn build_cli() -> Command {
     Command::new("will")
+        .arg_required_else_help(true)
         .version("0.1.0")
         .author("uimaxbai (https://gitlab.com/uimaxbai)")
         .about("A package manager that willingly installs packages.")
@@ -11,10 +12,18 @@ pub fn build_cli() -> Command {
                 .long("generate")
                 .action(ArgAction::Set)
                 .value_parser(value_parser!(Shell)))
-        .arg(Arg::new("install")
-                )
+        .arg(Arg::new("verbose")
+                .short('v')
+                .long("verbose")
+                .help("Be verbose.")
+                .action(ArgAction::SetTrue)) // No argument after flag
         .subcommand(Command::new("install")
+                        .arg_required_else_help(true)
                         .alias("i")
+                        .arg(Arg::new("package")
+                                .index(1)
+                                .help("The package you want to install.")
+                                .required(true))
                         .arg(Arg::new("source")
                                 .short('s')
                                 .long("from-source")
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,46 @@
-use std::io::{Result, stdout};
+use std::io::stdout;
 use clap::Command;
 use clap_complete::{generate, Generator, Shell};
+use std::collections::HashMap;
+use std::result::Result;
 
 extern crate serde_json;
 extern crate clap;
 extern crate clap_complete;
 
-pub mod record;
-pub mod request;
-pub mod cli;
+mod record;
+mod request;
+mod cli;
 
 fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
     generate(gen, cmd, cmd.get_name().to_string(), &mut stdout());
 }
 
-fn main() -> Result<()> {
-    let matches = cli::build_cli().get_matches();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = cli::build_cli();
+    let matches = cli.clone().get_matches();
+
+    /* let binding = cli.clone();
+    let positionals = binding.get_positionals(); */
 
     if let Some(generator) = matches.get_one::<Shell>("generator").copied() {
         let mut cmd = cli::build_cli();
         eprintln!("Generating completion file for {generator}...");
         print_completions(generator, &mut cmd);
+    }
+
+    if let Some(matches) = matches.subcommand_matches("install") {
+        if let Some(package) = matches.get_one::<String>("package") {
+            println!("You want to install, {}!", package);
+            let url = format!("https://will.okit.works/package?p={package}");
+            // TODO fix
+            let resp = reqwest::get(url)
+                .await?
+                .json::<HashMap<String, String>>()
+                .await?;
+            println!("{resp:#?}");
+        }
     }
     
     // let iterator = matches.values_of("something");
